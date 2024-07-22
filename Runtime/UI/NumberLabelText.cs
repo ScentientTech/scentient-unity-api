c71910dad53840c163946d98dc57aed9442a5a83
Paul Hayes <paul.hayes@entropedia.co.uk>
2024-07-22 11:31:48 +0100

    Reorganised files Moved helper UI component into Runtime/UI as it's referenced by multiple samples.

---
+++ Runtime/UI/NumberLabelText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scentient 
+{
+[RequireComponent(typeof(Text))]
+public class NumberLabelText : MonoBehaviour
+{
+    [SerializeField] Text m_textField;
+    [SerializeField] string m_formatString = "{0:0.0}";
+
+    void Reset()
+    {
+        m_textField = GetComponent<Text>();
+    }
+
+    public void SetValue(float value)
+    {
+        m_textField.text = string.Format(m_formatString,value);
+    }
+}
+
+}
+
