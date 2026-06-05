507aa0da2eb40c0d07b96be4daf97d7f4210e67c
Paul Hayes <paul.hayes@entropedia.co.uk>
2026-06-05 12:24:27 +0100

    Created examples dir for shared code between the different examples.

--- Runtime/Examples/UI/NumberLabelText.cs
+++ Runtime/Examples/UI/NumberLabelText.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Scentient 
+namespace Scentient.Examples 
 {
 [RequireComponent(typeof(Text))]
 public class NumberLabelText : MonoBehaviour
